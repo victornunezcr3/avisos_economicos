71e7474b7f2210bb77c10f0e1af16cb282b351f7
victornunezcr3 <victornunezcr@gmail.com>
2022-02-10 22:01:11 -0600

    filtros y estilo

---
+++ dist/index.80751d1a.js
@@ -0,0 +1,92 @@
+const autos = [
+    {
+        tipo: 'venta',
+        descripcion: 'Computadora de segunda marca dell con monitor',
+        precio: 50000,
+        sector: 'computo',
+        lugar: 'centro',
+        notas: 'Negociable con Jose al 8888-8888'
+    },
+    {
+        tipo: 'compra',
+        descripcion: 'CPU Hewlett Packcard',
+        precio: 75000,
+        sector: 'computo',
+        lugar: 'angeles',
+        notas: 'Con Mario al 8888-8888'
+    },
+    {
+        tipo: 'venta',
+        descripcion: 'Monitor de 20 pulgadas AOC como nuevo',
+        precio: 50000,
+        sector: 'computo',
+        lugar: 'centro',
+        notas: 'llamar a juan B al 2764-0000'
+    },
+    {
+        tipo: 'venta',
+        descripcion: 'Celular huawey y7 2018',
+        precio: 45000,
+        sector: 'celulares',
+        lugar: 'tucan',
+        notas: 'Llamar a Francisca Ortega'
+    },
+    {
+        tipo: 'venta',
+        descripcion: 'Taladro percutor marca gato',
+        precio: 30000,
+        sector: 'herramientas',
+        lugar: 'centro',
+        notas: 'llamar a patricio al 8888-8888'
+    },
+    {
+        tipo: 'compra',
+        descripcion: 'Peon de albañileria por horas',
+        precio: 1200,
+        sector: 'empleo',
+        lugar: 'centro',
+        notas: 'Contactar a Horacio 8888-8888, urge'
+    },
+    {
+        tipo: 'venta',
+        descripcion: 'Hago costuras todo tipo, cobro por pieza',
+        precio: 1000,
+        sector: 'costura',
+        lugar: 'oasis',
+        notas: 'contactar a mariela moncada 8888-8888'
+    },
+    {
+        tipo: 'venta',
+        descripcion: 'Servicio de taxi, zona rio frio',
+        precio: 1,
+        sector: 'transporte',
+        lugar: 'centro',
+        notas: 'llamar a bernardo al 8888-8888'
+    },
+    {
+        tipo: 'venta',
+        descripcion: 'Motocicleta marca gato 125cc',
+        precio: 50000,
+        sector: 'motocicleta',
+        lugar: 'oasis',
+        notas: 'sin papeles, llamar 88888888, negociable'
+    },
+    {
+        tipo: 'venta',
+        descripcion: '1000 metros cuadrados de terreno',
+        precio: 10000000,
+        sector: 'varios',
+        lugar: 'vegas',
+        notas: 'diez millones, con escritura, llamar a joselito 27640000'
+    },
+    {
+        tipo: 'venta',
+        descripcion: 'Alquiler de casa de habitacion 2 cuartos',
+        precio: 50000,
+        sector: 'alquileres',
+        lugar: 'centro',
+        notas: 'Negociable, con maria al 88888888'
+    }
+];
+
+//# sourceMappingURL=index.80751d1a.js.map
---
+++ src/js/app.js
@@ -0,0 +1,107 @@
+// Selectores
+const tipo = document.querySelector('#tipo');
+const lugar = document.querySelector('#lugar');
+const sector = document.querySelector('#sector');
+
+
+// Datos para la busqueda
+const datosBusqueda = {
+    tipo : '',
+    lugar: '',
+    sector:''
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    mostrarAutos(autos);
+});
+
+// Event Listeners para el formulario
+tipo.addEventListener('input', e => {
+    datosBusqueda.tipo = e.target.value;
+
+    // Mandar llamar la función de filtrar Autos
+    filtrarAuto();
+});
+
+sector.addEventListener('input', e => {
+    datosBusqueda.sector = e.target.value
+    // Mandar llamar la función de filtrar Autos
+    filtrarAuto();
+});
+
+lugar.addEventListener('input', e => {
+    datosBusqueda.lugar = e.target.value
+    // Mandar llamar la función de filtrar Autos
+    filtrarAuto();
+});
+
+function limpiarHTML() {
+    // Leer el elemento Resultado
+    const contenedor = document.querySelector('#resultado');
+
+    // limpiar los resultados anteriores
+    while(contenedor.firstChild) {
+        contenedor.removeChild(contenedor.firstChild);
+    }
+}
+
+function mostrarAutos(autos){
+    limpiarHTML();
+
+    // Leer el elemento Resultado
+    const contenedor = document.querySelector('#resultado');
+
+    // Construir el HTML de los autos
+    autos.forEach(auto => {
+        const autoHTML = document.createElement('p');
+        //autoHTML.className ='card body';
+        //autoHTML.classList.add('alert alert-success');
+        autoHTML.innerHTML = `
+            <p>${auto.tipo} - ${auto.sector} - ${auto.descripcion} - Precio: ${auto.precio} - Direccion: ${auto.lugar} - Contacto: ${auto.notas}</p><br/>
+        `;
+        contenedor.appendChild(autoHTML);
+
+    })
+}
+function noResultado() {
+    limpiarHTML();
+
+    const noResultado = document.createElement('div');
+    noResultado.classList.add('alerta', 'error');
+    noResultado.appendChild(document.createTextNode('No hay Resultados'));
+    document.querySelector('#resultado').appendChild(noResultado);
+}
+
+function filtrarAuto() {
+   const resultado = autos.filter(filtrarMarca).filter(filtrarPuertas).filter(filtrarTransmision);
+
+//    console.log(resultado);
+   if(resultado.length){
+        mostrarAutos(resultado);
+   } else {
+       noResultado();
+   }
+}
+
+
+// Aplica los filtros
+function filtrarMarca(auto) {
+    if(datosBusqueda.tipo){
+        return auto.tipo === datosBusqueda.tipo;
+    } 
+    return auto;
+}
+
+function filtrarPuertas(auto) {
+    if(datosBusqueda.sector){
+        return auto.sector === datosBusqueda.sector;
+    }
+    return auto;
+}
+
+function filtrarTransmision(auto) {
+    if(datosBusqueda.lugar){
+        return auto.lugar === datosBusqueda.lugar;
+    } 
+    return auto;
+}

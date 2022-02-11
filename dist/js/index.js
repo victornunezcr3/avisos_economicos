d90e07535aa6f91856c3fbb64ffe38f4c60b2267
victornunezcr3 <victornunezcr@gmail.com>
2022-02-11 14:55:09 -0600

    produccion

---
+++ dist/js/index.js
@@ -0,0 +1,11 @@
+function $parcel$export(e, n, v, s) {
+  Object.defineProperty(e, n, {get: v, set: s, enumerable: true, configurable: true});
+}
+
+$parcel$export(module.exports, "add", function () { return $0d5c25f0671df88f$export$e16d8520af44a096; });
+function $0d5c25f0671df88f$export$e16d8520af44a096(a, b) {
+    return a + b;
+}
+
+
+//# sourceMappingURL=index.js.map

737276d13784c2ae7a17e59561c9c0f13cedcb2a
victornunezcr3 <victornunezcr@gmail.com>
2022-02-15 14:16:10 -0600

    build app

--- dist/index.80751d1a.js
+++ dist/index.80751d1a.js
@@ -2,146 +2,38 @@ const aviso = [
     {
         consec: 'aefd01',
         tipo: 'venta',
-        descripcion: 'Computadora de segunda marca dell con monitor',
-        precio: 50000,
+        descripcion: 'Reparación y mantenimiento de equipo de computo',
+        precio: 0,
         sector: 'computo',
         lugar: 'centro',
-        notas: 'Negociable con Jose al 8888-8888'
+        notas: 'Revisión gratis!!!, presupuesto sujeto a revisión. Instalacion de software con licencia, Contacto: Victor Ml. Nuñez al 8554-6767'
     },
     {
         consec: 'aefd-2',
-        tipo: 'compra',
-        descripcion: 'CPU Hewlett Packcard',
-        precio: 75000,
+        tipo: 'venta',
+        descripcion: 'Desarrollo de sitios web personalizados',
+        precio: 0,
         sector: 'computo',
-        lugar: 'angeles',
-        notas: 'Con Mario al 8888-8888'
+        lugar: 'centro',
+        notas: 'Presupuesto de acuerdo a la necesidad. Contacto: Victor Ml. Nuñez, teléfono: 8554-6767'
     },
     {
         consec: 'aefd-3',
         tipo: 'venta',
-        descripcion: 'Monitor de 20 pulgadas AOC como nuevo',
-        precio: 50000,
+        descripcion: 'Desarrollo de plantillas en EXCEL',
+        precio: 0,
         sector: 'computo',
         lugar: 'centro',
-        notas: 'llamar a juan B al 2764-0000'
+        notas: 'Precio de acuerdo a la necesidad, Contacto: Victor Ml. Nuñez, teléfono: 8554-6767'
     },
     {
         consec: 'aefd-4',
         tipo: 'venta',
-        descripcion: 'Celular huawey y7 2018',
-        precio: 45000,
-        sector: 'celulares',
-        lugar: 'tucan',
-        notas: 'Llamar a Francisca Ortega'
-    },
-    {
-        consec: 'aefd-5',
-        tipo: 'venta',
-        descripcion: 'Taladro percutor marca gato',
-        precio: 30000,
-        sector: 'herramientas',
-        lugar: 'centro',
-        notas: 'llamar a patricio al 8888-8888'
-    },
-    {
-        consec: 'aefd-6',
-        tipo: 'empleo',
-        descripcion: 'Peon de albañileria por horas',
-        precio: 1200,
-        sector: 'empleo',
-        lugar: 'centro',
-        notas: 'Contactar a Horacio 8888-8888, urge'
-    },
-    {
-        consec: 'aefd-7',
-        tipo: 'venta',
-        descripcion: 'Hago costuras todo tipo, cobro por pieza',
-        precio: 1000,
-        sector: 'costura',
-        lugar: 'oasis',
-        notas: 'contactar a mariela moncada 8888-8888'
-    },
-    {
-        consec: 'aefd-8',
-        tipo: 'venta',
-        descripcion: 'Servicio de taxi, zona rio frio',
-        precio: 1,
-        sector: 'transporte',
-        lugar: 'centro',
-        notas: 'llamar a bernardo al 8888-8888'
-    },
-    {
-        consec: 'aefd-9',
-        tipo: 'venta',
-        descripcion: 'Motocicleta marca gato 125cc',
-        precio: 50000,
-        sector: 'motocicleta',
-        lugar: 'oasis',
-        notas: 'sin papeles, llamar 88888888, negociable'
-    },
-    {
-        consec: 'aefd-10',
-        tipo: 'venta',
-        descripcion: '1000 metros cuadrados de terreno',
-        precio: 10000000,
-        sector: 'varios',
-        lugar: 'vegas',
-        notas: 'diez millones, con escritura, llamar a joselito 27640000'
-    },
-    {
-        consec: 'aefd-11',
-        tipo: 'venta',
-        descripcion: 'casa de habitacion 2 cuartos',
-        precio: 5000000,
-        sector: 'varios',
-        lugar: 'centro',
-        notas: ' 5 millones Negociable, con maria al 88888888'
-    },
-    {
-        consec: 'aefd-12',
-        tipo: 'alquila',
-        descripcion: 'Apartamento para persona sola, sin mascotas ni niños',
-        precio: 1200,
+        descripcion: 'Fotocopias b/n y color',
+        precio: 0,
         sector: 'servicio',
-        lugar: 'tucan',
-        notas: 'Contactar a Serapio 8888-8888, urge'
-    },
-    {
-        consec: 'aefd-13',
-        tipo: 'alquila',
-        descripcion: 'Casa habitacion 3 cuartos como nueva',
-        precio: 1200,
-        sector: 'servicio',
-        lugar: 'tucan',
-        notas: 'Contactar a Serapio 8888-8888, urge aefd-13'
-    },
-    {
-        consec: 'aefd-14',
-        tipo: 'empleo-o',
-        descripcion: 'Jornalero por horas para papaya',
-        precio: 1200,
-        sector: 'servicio',
-        lugar: 'tucan',
-        notas: 'Contactar a Serapio 8888-8888, urge aefd-14'
-    },
-    {
-        consec: 'aefd-15',
-        tipo: 'empleo-b',
-        descripcion: 'Jornalero por horas',
-        precio: 1200,
-        sector: 'servicio',
-        lugar: 'tucan',
-        notas: 'Contactar a Serapio 8888-8888, urge aefd-15'
-    },
-    {
-        consec: 'aefd-16',
-        tipo: 'venta',
-        descripcion: 'Llave maya de 32 G estandard 3.1 ***nuevas***',
-        precio: 1200,
-        sector: 'varios',
         lugar: 'centro',
-        notas: 'Contactar a Victor ml nunez 8888-8888, oportunidad  id=aefd-16'
+        notas: 'Centro de Finca diez, Con Noelia Mantilla al 6110-1115'
     }
 ];
 
